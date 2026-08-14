f83201862a0a68e1198c8c46097dff68ea5f1f97
sarvarbekcoder0601-arch <sarvarbekcoder0601@gmail.com>
2026-08-14 12:03:37 +0500

    fix api cors with proxy

--- src/service/Api.js
+++ src/service/Api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { getItem } from "../components/helpers/Persistance-storage";
 
-axios.defaults.baseURL = 'https://corsproxy.io/?https://api.realworld.io/api'
-axios.interceptors.request.use(config => {
-    const token = getItem('token')
-    const authorization = token ? `Token ${token}` : ''
-    config.headers.Authorization = authorization
-    return config
+axios.defaults.baseURL = 'https://corsproxy.io/?https://api.realworld.io/api';
 
-})
-export default axios
+axios.interceptors.request.use(config => {
+    const token = getItem('token');
+    const authorization = token ? `Token ${token}` : '';
+    config.headers.Authorization = authorization;
+    return config;
+});
+
+export default axios;
