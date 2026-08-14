5cbcc7e4aa84a47ce09b93b1b10e34b4fb6d2820
sarvarbekcoder0601-arch <sarvarbekcoder0601@gmail.com>
2026-08-14 13:00:09 +0500

    fix: switch api and handle register errors safely

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,7 +4,7 @@ import { Input } from '../ui'
 import { useDispatch, useSelector } from "react-redux";
 import { signUserStart, signUserSuccess, signUserFailure } from "../slice/Auth";
 import AuthService from "../service/Auth";
-import {ValidationError } from "./";  
+import { ValidationError } from "./";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
@@ -16,52 +16,51 @@ const Register = () => {
   const { isLoading, loggedIn } = useSelector(state => state.auth)
   const navigate = useNavigate()
 
-  const registerHandler = async e => { 
+  const registerHandler = async e => {
     e.preventDefault()
     dispatch(signUserStart())
-    const user = {username: name, email, password}
+    const user = { username: name, email, password }
 
     try {
       const response = await AuthService.userRegister(user)
       dispatch(signUserSuccess(response.user))
       navigate('/')
     } catch (error) {
-      console.log(error.response.data);
-      
-      dispatch(signUserFailure(error.response.data.errors))
+      console.log(error.response?.data);
+      dispatch(signUserFailure(error.response?.data?.errors || { message: ['Something went wrong. Please try again.'] }))
     }
+}
+
+useEffect(() => {
+  if (loggedIn) {
+    navigate('/')
   }
+}, [loggedIn])
 
-  useEffect (() => {
-    if(loggedIn) {
-      navigate('/')
-    }
-  }, [loggedIn])
+return (
+  <div className="text-center mt-3">
+    <main className="form-signin col-11 col-sm-8 col-md-6 col-lg-4 mx-auto">
+      <form onSubmit={registerHandler}>
+        <img
+          className="mb-2"
+          src={icon}
+          alt=""
+          width="50"
+          height="50"
+        />
+        <h1 className="h3 mb-3 fw-normal">Please Register</h1>
+        <ValidationError />
+        <Input label={'Username'} state={name} setState={setName} />
+        <Input label={'Email address'} type={"email"} state={email} setState={setEmail} />
+        <Input label={'Password'} type={"password"} state={password} setState={setPassword} />
 
-  return (
-    <div className="text-center mt-3">
-      <main className="form-signin col-11 col-sm-8 col-md-6 col-lg-4 mx-auto">
-        <form onSubmit={registerHandler}>
-          <img
-            className="mb-2"
-            src={icon}
-            alt=""
-            width="50"
-            height="50"
-          />
-          <h1 className="h3 mb-3 fw-normal">Please Register</h1>
-          <ValidationError/>
-          <Input label={'Username'} state={name} setState={setName}/>
-          <Input label={'Email address'} type={"email"} state={email} setState={setEmail}/>
-          <Input label={'Password'} type={"password"} state={password} setState={setPassword}/>
-
-          <button className="btn btn-primary w-100 py-2 mt-2" type="submit" disabled={isLoading}>
-            {isLoading ? 'Loading...' : 'Register '}
-          </button>
-        </form>
-      </main>
-    </div>
-  );
+        <button className="btn btn-primary w-100 py-2 mt-2" type="submit" disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Register '}
+        </button>
+      </form>
+    </main>
+  </div>
+);
 };
 
 export default Register;
